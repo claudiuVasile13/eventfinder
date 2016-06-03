fa3421792556be261b927a7a5d8b5ba65999f4fb
Elisav <claudiu.vasile13@yahoo.com>
2016-06-04 01:06:32 +0300

    design 4

--- js/menu.js
+++ js/menu.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     $('#res-icon-menu').on('click', function() {
-        $('#side-menu').css('left', '0px');
-    });
-    $('#close').on('click', function() {
-        $('#side-menu').css('left', '-100%');
+        $('#side-menu').toggleClass('active inactive');
+        var oldimg = $(this).attr('src');
+        var img = (oldimg === '../img/menu.png') ? '../img/close.png' : '../img/menu.png';
+        $(this).attr('src', img);
     });
 });
 
