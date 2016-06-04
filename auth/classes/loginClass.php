0a3e430d35933e7f29dae2c52019e039773d8e80
TTraian <tanase_traian95@yahoo.com>
2016-06-04 17:04:47 +0300

    Session function

--- auth/classes/loginClass.php
+++ auth/classes/loginClass.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
  * Description of loginClass
  *
@@ -8,38 +9,49 @@
 require '../../config/dbOperations.php';
 
 class loginClass {
-    
+
     private $email;
     private $password;
-    
-    function login(){
+
+    function login() {
 //        get data from POST
-      $this->email = $_POST["email"];
-      $this->password = $_POST["pass"];
+        $this->email = $_POST["email"];
+        $this->password = $_POST["pass"];
 //Connect to database
-      $dbOpp =  new dbOperations();
-      $dbOpp->connection();
-      
+        $dbOpp = new dbOperations();
+        $dbOpp->connection();
+
 // query database 
-      $conditions = "WHERE user_email='$this->email' AND user_password='$this->password'";
-      $rows = count($dbOpp->select('users','*', $conditions));
-      if($rows){
-          if(isset($_POST["remember"])){
-                 loginClass::cookie();
-      }
-          header("Location: ../../view/index.php");
-      }
-      else{
-          header("Location: ../../view/newlogin.php");
-      }
+        $conditions = "WHERE user_email='$this->email' AND user_password='$this->password'";
+        $rows = count($dbOpp->select('users', '*', $conditions));
+        if ($rows) {
+            if (isset($_POST["remember"])) {
+                loginClass::setCookie();
+            } 
+            else{
+                loginClass::startSession();
+            }
+            header("Location: ../../view/index.php");
+        }
+        else {
+            header("Location: ../../view/login.php");
+        }
     }
-    
+
     // Create a cookie
-    static function cookie(){
+    static function setCookie() {
         $cookie_email = $_POST["email"];
-        setcookie("isLoggedIn",$cookie_email, time() + (60), "/");
+        setcookie("isLoggedIn", $cookie_email, time() + (60), "/");
     }
-    
+
+    //Create a seesion
+    static function startSession() {
+        session_start();
+        $session_data = $_POST["email"];
+        $_SESSION["isLoggedIn"] = "true";
+        $_SESSION["email"] = $session_data;
+    }
+
 }
 
 $login = new loginClass();
