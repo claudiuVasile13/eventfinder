1ecc60a3de6089e54cb56c4189919d59206f05c3
Elisav <claudiu.vasile13@yahoo.com>
2016-06-07 19:03:08 +0300

    index design

--- view/footer.php
+++ view/footer.php
@@ -1,31 +1,7 @@
-<footer>
+<div id="footer-box">
     <div class="row footer" id="news-call">
-        <div class="col-lg-6" id="news">
-            <form id="news-form">
-                <h3>sign up to our newsletter</h3>
-                <img src="../img/news36.png" alt="E-mail" />
-                <input type="text" placeholder="name@host.domain" />
-                <button>SUBMIT</button>
-            </form>
-        </div>
-        <div class="col-lg-6" id="call">
-            <h3>Give Us A Call</h3>
-            <img src="../img/call.png" alt="CALL" />
-            <p>+123 456 7890</p>
+        <div class="col-lg-12" id="news">
         </div>
     </div>
-    <div class="row footer" id="copy-media">
-        <div class="col-lg-9" id="copy">
-            <img id="logo-img" src="https://www.w3.org/html/logo/downloads/HTML5_1Color_White.png" alt="LOGO" />
-            <img src="../img/copy.png" alt="copyright" />
-            <p>2016 <span id="eventfinder">eventFinder</span>.All rights reserved.</p>
-        </div> 
-        <div class="col-lg-3" id="media">
-            <div id="media-box">
-                <a href="#" target="_blank"><img src="../img/facebook.png" alt="facebook" /></a>
-                <a href="#" target="_blank"><img src="../img/google.png" alt="google" /></a>
-                <a href="#" target="_blank"><img src="../img/linkedin.png" alt="linkedin" /></a>
-                <a href="#" target="_blank"><img src="../img/twitter.png" alt="twitter" /></a>
-            </div>
-        </div>
-</footer>
+</div>
+
--- view/header.php
+++ view/header.php
@@ -1,24 +1,24 @@
-<header>
-    <!--<h1 id="logo-h">eventFinder</h1>-->
+<div id="header" data-position="fixed">
     <img id="logo-img" src="https://www.w3.org/html/logo/downloads/HTML5_1Color_White.png" alt="LOGO">
     <img id="res-icon-menu" src="../img/menu.png" alt="Menu" />
     <ul id="menu">
-        <li><a href="index.php">Home</a></li>
-        <li><a a href="#">About</a></li>
-        <li><a a href="buy.php">Events</a></li>
-        <li><a a href="login.php">Account</a></li>
-        <li><a a href="contact.php">Contact</a></li>
+        <li><a href="index.php?page=index">Home</a></li>
+        <li><a href="about.php?page=about">About</a></li>
+        <li><a href="buy.php?page=buy">Events</a></li>
+        <li><a href="login.php?page=login">Account</a></li>
+        <li><a href="contact.php?page=contact">Contact</a></li>
     </ul>
     <div id="clear-header"></div>
-</header>
 
-<div id="side-menu">
-    <ul id="side-menu-list">
-        <li><a href="#">Home</a></li>
-        <li><a a href="#">About</a></li>
-        <li><a a href="#">Events</a></li>
-        <li><a a href="#">Account</a></li>
-        <li><a a href="#">Contact</a></li>
-    </ul>
+    <div id="side-menu">
+        <ul id="side-menu-list">
+            <li><a href="index.php?page=index">Home</a></li>
+            <li><a href="about.php?page=about">About</a></li>
+            <li><a href="buy.php?page=buy">Events</a></li>
+            <li><a href="login.php?page=login">Account</a></li>
+            <li><a href="contact.php?page=contact">Contact</a></li>
+        </ul>
+    </div>
 </div>
+
 
