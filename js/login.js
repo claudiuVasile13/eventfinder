363ac0aef59a2f10fb7522f181fc3f296817c89d
TTraian <tanase_traian95@yahoo.com>
2016-06-09 21:15:20 +0300

    check if pass and repass are the same

--- js/login.js
+++ js/login.js
@@ -1,10 +1,24 @@
-$(document).ready(function() {
-    $('.fields').blur(function() {
-      var $this = $(this);
-      if ($this.val())
-        $this.addClass('used');
-      else
-        $this.removeClass('used');
+$(document).ready(function () {
+    $('.fields').blur(function () {
+        var $this = $(this);
+        if ($this.val())
+            $this.addClass('used');
+        else
+            $this.removeClass('used');
+    });
+
+    //function to recognize if pass and re-pass have the same value
+    $("#repass_r").focusout(function () {
+        var pass = $("#pass_r").val();
+        var repass = $("#repass_r").val();
+        var email = $("#email_r").val();
+        if (email !== '') {
+            if (pass === repass) {
+                alert("e la fel");
+            } else {
+                alert("parola e diferita");
+            }
+        }
     });
 });
 
