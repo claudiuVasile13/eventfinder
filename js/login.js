e6a644dc2a046eeb7481e3d606263cc8a48b786a
TTraian <tanase_traian95@yahoo.com>
2016-06-10 17:51:37 +0300

    Color border bottom of input in register if fields are ok

--- js/login.js
+++ js/login.js
@@ -8,17 +8,32 @@ $(document).ready(function () {
     });
 
     //function to recognize if pass and re-pass have the same value
-    $("#repass_r").focusout(function () {
+    $("#repass_r").keyup(function () {
         var pass = $("#pass_r").val();
         var repass = $("#repass_r").val();
         var email = $("#email_r").val();
         if (email !== '') {
             if (pass === repass) {
-                alert("e la fel");
+                
+                $(this).removeClass("invalid");
+                $(this).addClass("valid");
             } else {
-                alert("parola e diferita");
+                $(this).removeClass("valid");
+                $(this).addClass("invalid");
             }
         }
+    });
+    
+    $("#pass_r").keyup(function(){
+       var pass = $(this).val();
+       var length_pass = pass.length;
+       if(length_pass<6){
+                $(this).removeClass("valid");
+                $(this).addClass("invalid");
+            } else {
+                $(this).removeClass("invalid");
+                $(this).addClass("valid");
+            }
     });
 });
 
