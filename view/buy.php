29390fac4b6695edfa86308dd17c5ea6cddcc417
Elisav <claudiu.vasile13@yahoo.com>
2016-07-05 18:40:15 +0300

    offer design

--- view/buy.php
+++ view/buy.php
@@ -5,13 +5,33 @@
         <meta charset="UTF-8">
         <title>Buy</title>
         <link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
-        <link rel="stylesheet" type="text/css" href="../css/header.css">
+        <link rel="stylesheet" type="text/css" href="../css/buy.css">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:300' rel='stylesheet' type='text/css'>
     </head>
     <body>
-        <!-- Header -->
-        <?php require_once 'header.php'; ?>
-        
+        <div id="main-box">
+            <div id="box-container">
+                <div id="offer-box">
+                    <div id="description-box">
+                        <h3 class="name">Offer's Name</h3>
+                        <p class="offer-description">
+                            dasdas adasdas dasdas asdas dasdas adasd adsadas adsdas adasdas adas adsasda
+                            asdasddas asdasdasd asdsadsad dasdasdasd asdsada asdasd asdsadasd asdasdasdas
+                            asdasdasd. dasdasd asdsad dasdsad asdsdasd dasdsada adsdsadasda hghfghfg gf
+                            dasdgrgdf fwefw fwefew wfefwef wefewf wf ew  efwewfew wfwefwef wfwef wfwefwef
+                            gergergr ergegerger ergergerge egergerge egerger egergerge egergeg eegregre 
+                            gregerger ergergreger egergrege egergerge ergerger egregre egerger ergerg
+                            gegergre ergregerge erggr.
+                        </p>
+                        <p class="owner">By Owner of the offer</p>
+                    </div>
+                    <div id="info">
+                        <div class="currentImg" style="background-image: url('../img/subway.png');"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
 
